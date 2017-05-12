957c34a514b6a99bbfe87bae378313347886be84
aisensiy <aisensiy@163.com>
2017-05-12 13:42:30 +0800

    a basic setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    let { todos } = this.props;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <ul className="todos">
+         {todos.map((todo, idx) => <li key={idx} className={todo.completed ? 'completed' : ''}>{todo.title}</li>)}
+       </ul>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  todos: PropTypes.arrayOf({
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired
+  })
+};
+
+App.defaultProps = {
+  todos: [
+    {
+      title: 'what to do',
+      completed: false
+    }, 
+    {
+      title: 'what to do next',
+      completed: true
+    }
+  ]
+};
 
 export default App;
